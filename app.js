355929f62a11dfc1a6fa1050ca709a2184b151ce
farzan190 <farzanxavier@gmail.com>
2024-02-15 15:53:12 +0530

    updated center btn

---
+++ app.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tabs-project'
